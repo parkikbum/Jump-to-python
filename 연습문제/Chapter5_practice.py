8775525e823b220914d5dcb8870fdf0c681bf7b6
parkikbum <pibum@naver.com>
2021-02-09 01:20:26 +0900

    연습문제

--- 연습문제/Chapter5_practice.py
+++ 연습문제/Chapter5_practice.py
@@ -1,2 +1,18 @@
 #5장 연습문제
-#왜안돼dd
+
+#1번문제
+class UpgradeCalculator:
+    def __init__(self):
+        self.value = 0
+
+    def add(self, val):
+        self.value += val
+
+    def minus(self, val):
+        self.value -= val
+
+cal = UpgradeCalculator()
+cal.add(10)
+cal.minus(7)
+
+print(cal.value)
