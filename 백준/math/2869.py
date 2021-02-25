bf69748ef484fbcc78aed027235185e9c7e515c5
parkikbum <pibum@naver.com>
2021-02-25 17:39:58 +0900

    python

---
+++ 백준/math/2869.py
@@ -0,0 +1,6 @@
+import sys
+a,b,v = sys.stdin.readline().split()
+if (int(v)-int(b))%(int(a)-int(b)) == 0:
+    print((int(v)-int(b))//(int(a)-int(b)))
+else:
+    print((int(v) - int(b))//(int(a) - int(b)) + 1)
