ffee94fcf944a0f793c052e8cbfeb72335d9b8ba
parkikbum <pibum@naver.com>
2021-03-05 20:44:40 +0900

    python

---
+++ 백준/math/3053.py
@@ -0,0 +1,3 @@
+r = float(input())
+print(round(r*r*3.14159265358979,6))
+print(round(r*r*2,6))
---
+++ 백준/python/1018.py
@@ -0,0 +1,8 @@
+import sys
+m,n = map(int, sys.stdin.readline().split())
+l = []
+ck = []
+cnt = 0
+i = 0
+for x in range(m):
+    l.append(input())
