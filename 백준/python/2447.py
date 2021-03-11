2e1619854cf7219d5b7628db8c759d45befe8a56
parkikbum <pibum@naver.com>
2021-03-11 20:33:04 +0900

    python

--- 백준/python/2447.py
+++ 백준/python/2447.py
@@ -1,3 +1,14 @@
 n = int(input())
-
-def star():
+stars = [[' ' for _ in range(n)]for _ in range(n)]
+def star(size, x, y):
+    if size == 1:
+        stars[y][x] = '*'
+    else:
+        next_size = size//3
+        for dy in range(3):
+            for dx in range(3):
+                if dy != 1 or dx != 1:
+                    stars(next_size,x+dx*next_size, y+dy*next_size)
+stars(n,0,0)
+for k in stars:
+    print(''.join(k))
