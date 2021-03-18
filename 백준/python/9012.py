d16c51cd29504407a862909cb52f8f68cd10469a
parkikbum <pibum@naver.com>
2021-03-18 15:17:17 +0900

    python

---
+++ 백준/python/9012.py
@@ -0,0 +1,13 @@
+t = int(input())
+ck = []
+cnt = []
+sol = []
+for x in range(t):
+    vps = input()
+    ck = list(vps)
+    if ck[0] == ')':
+        sol.append("NO")
+    for l in ck:
+        for c
+
+
