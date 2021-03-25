c51817d621cc67be343f1204403df5caae2ccd28
parkikbum <pibum@naver.com>
2021-03-25 21:19:44 +0900

    python

--- 백준/python/7568.py
+++ 백준/python/7568.py
@@ -1,22 +1,16 @@
 n = int(input())
 dc = []
-dc2 = []
-rank = []
-for i in range(n):
+for k in range(n):
     x,y = map(int, input().split())
     dc.append((x,y))
-a = n
-for k in dc:
-    for j in dc:
-        if k[0] > j[0]:
-            if k[1] > j[1]:
-                a = a - 1
-    rank.append(a)
-    a = 5
-for v in rank:
-    if rank.count(v) == 1:
-        print(v,end = ' ')
-    else:
-        print(n-rank.count(v),end=' ')
-
-
+grade = []
+for l in range(n):
+    grade.append(int(1))
+for i in range(n):
+    for j in range(i+1, n):
+        if dc[i][0] < dc[j][0] and dc[i][1] < dc[j][1]:
+            grade[i] = grade[i] + 1
+        elif dc[i][0] > dc[j][0] and dc[i][1] > dc[j][1]:
+            grade[j] = grade[j] + 1
+for a in grade:
+    print(a,end=" ")
