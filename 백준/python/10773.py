e8c639d94fa9c54e0f79de58d6bbd7018da2775e
parkikbum <pibum@naver.com>
2021-03-26 13:06:52 +0900

    python

---
+++ 백준/python/10773.py
@@ -0,0 +1,11 @@
+k = int(input())
+num = []
+n = 0
+for x in range(k):
+    n = int(input())
+    if n == 0:
+        num.pop(len(num)-1
+                )
+    if n != 0:
+        num.append(n)
+print(sum(num))
