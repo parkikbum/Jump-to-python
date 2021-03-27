734aaa75bdba49480048e5b37e6012098884146d
parkikbum <pibum@naver.com>
2021-03-27 12:46:32 +0900

    python

---
+++ 백준/python/10989.py
@@ -0,0 +1,10 @@
+import sys
+n = int(sys.stdin.readline())
+num_ls = [0] * 10001
+for i in range(n):
+    num = int(sys.stdin.readline())
+    num_ls[num] = num_ls[num]+1
+for k in range(10001):
+    if num_ls[k] != 0:
+        for j in range(num_ls[i]):
+            print(k)
