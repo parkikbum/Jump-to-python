946f09e73ea3b4f397c9fed6ce09918a5e309cd0
parkikbum <pibum@naver.com>
2021-03-30 00:59:38 +0900

    python

---
+++ 백준/python/15649.py
@@ -0,0 +1,17 @@
+import sys
+n,m = map(int, input().split())
+visited = [False] * n
+out = []
+
+def solve(depth,n,m):
+    if depth == m:
+        print(' '.join(map(str, out)))
+        return
+    for i in range(len(visited)):
+        if not visited[i]:
+            visited[i] = True
+            out.append(i+1)
+            solve(depth+1, n,m)
+            visited[i] = False
+            out.pop()
+solve(0,n,m)
