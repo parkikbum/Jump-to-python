42113a5bb85c69a97c706a81649b9da9a72d7f6b
parkikbum <pibum@naver.com>
2021-04-04 23:45:27 +0900

    python

---
+++ 백준/math/2740.py
@@ -0,0 +1,13 @@
+f = lambda: map(int,input().split())
+n, m = f()
+a = [list(f()) for _ in range(n)]
+m, k = f()
+b = [list(f()) for _ in range(m)]
+c = [[0]*k for _ in range(n)]
+
+for i in range(n):
+    for j in range(k):
+        for h in range(m):
+            c[i][j]+=a[i][h]*b[h][j]
+
+for x in range(n): print(*c[x])
