e1df21d95db4837be84dd9de3608c642bc93baad
parkikbum <pibum@naver.com>
2021-04-08 19:26:50 +0900

    python

---
+++ 백준/python/17298.py
@@ -0,0 +1,15 @@
+import sys
+n = int(sys.stdin.readline())
+a = []
+a = list(map(int, sys.stdin.readline().split().strip()))
+stack = []
+for i in range(n):
+    while stack and a[stack[-1]] < a[i]:
+        a[stack[-1]] = a[i]
+        stack.pop()
+    stack.append(i)
+while stack:
+    a[stack[-1]] = -1
+    stack.pop()
+for i in a:
+    print(i, end=' ')
