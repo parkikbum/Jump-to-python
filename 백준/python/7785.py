75454f3c2901130c48a3aa16c7be56e4c8576b2c
parkikbum <pibum@naver.com>
2021-04-11 20:28:39 +0900

    python

--- 백준/python/7785.py
+++ 백준/python/7785.py
@@ -1,9 +1,11 @@
 import sys
 n = int(sys.stdin.readline())
-name_io = []
+nameIo = {}
 for x in range(n):
-    name_io.append(map(int, (sys.stdin.readline().split())))
-for k in len(name_io):
-    name_io.count(name_io[k][0]) 
-
-
+    name,inout = map(str, sys.stdin.readline().split())
+    nameIo[name] = inout
+    if nameIo[name] == 'leave':
+       del nameIo[name] 
+nameIo = sorted(nameIo, reverse=True)
+for k in nameIo:
+    print(k)
