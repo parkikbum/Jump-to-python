42d06e774b1190ff13035ce704ace03ebe0cc241
parkikbum <pibum@naver.com>
2021-04-22 22:27:49 +0900

    python

--- 백준/1759.py
+++ 백준/1759.py
@@ -1,10 +1,20 @@
 import sys
+from itertools import combinations
+mouem = ['a','e','i','o','u']
+sol = []
 l,c = map(int, sys.stdin.readline().split())
 passWord = list(map(str, sys.stdin.readline().split()))
 passWord.sort(key = lambda x : x[0])
-while 1:
-    for i in range(l):
-        print(passWord[i], end="")
-    passWord.append(passWord.pop(3))
-        
 
+for i in combinations(passWord, l):
+    word = ''.join(i)
+    vFlag = False
+    cFalg = 0
+    for i in word:
+        if i in mouem:
+            vFlag = True
+        else:
+            cFalg = cFalg + 1
+    if vFlag == True and cFalg >=2:
+        sol.append(word)
+print('\n'.join(sol))
