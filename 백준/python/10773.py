09e03f11c7a9bd49129ed1bb46c560dd768cfcb2
parkikbum <pibum@naver.com>
2021-04-24 20:57:25 +0900

    python

--- 백준/python/10773.py
+++ 백준/python/10773.py
@@ -4,8 +4,7 @@ n = 0
 for x in range(k):
     n = int(input())
     if n == 0:
-        num.pop(len(num)-1
-                )
+        num.pop(len(num)-1)
     if n != 0:
         num.append(n)
 print(sum(num))
